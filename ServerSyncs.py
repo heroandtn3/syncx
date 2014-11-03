2780e7f0253dc76fb68a8bfd31197cd0dbe63958
khanhoatink4 <khanhoait.bka@gmail.com>
2014-11-03 15:18:24 -0800

    update code

--- ServerSyncs.py
+++ ServerSyncs.py
@@ -33,45 +33,92 @@ class SocketFileServer(object):
         return os.sep.join(src_path.split(os.sep)[1:])
 
     def listen(self):
+        """
+        server listen host and port
+        app: Signature is "syncs"
+        buffer recv:
+            create: Create file
+                create|1|: directory "syncs|create|1|directory"
+                create|2|: file      "syncs|create|2|"
+            delete: Delete file
+                syncs|delete|1|directory
+                syncs|delete|2|file
+            modifed: modifed file
+                syncs|modifed|1|directory
+                syncs|modifed|2|file
+            moved: moved file
+                syncs|moved|1|directory
+                syncs|moved|2|file
+
+
+        """
         self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.soc.bind((self.host, int(self.port)))
         self.soc.listen(10)
 
+        logging.info("listening for connections, on PORT: %s", self.port)
+        self.conn, self.addr = self.soc.accept()
+        inputready,outputready,exceptready \
+            = select.select ([self.conn],[self.conn],[])
+        logging.info("Connect by %s", self.addr)
+
         while True:
-            logging.info("listening for connections, on PORT: %s", self.port)
-            self.conn, self.addr = self.soc.accept()
-            inputready,outputready,exceptready \
-                      = select.select ([self.conn],[self.conn],[])
-            logging.info("Connect by %s", self.addr)
-            datarecv = ""
+            datar = ""
             while 1:
                 data = self.conn.recv(1024)
-                datarecv += data.decode('utf-8')
-                if len(datarecv) > 0:
+                datar += data.decode('utf-8')
+                if len(datar) > 0:
                     break
+            logging.info(datar)
+            datar = datar.split("|")
 
-            datarecv = datarecv.split("|")
-            if datarecv[0] =="syncs" and datarecv[1] == "filename" and datarecv[3] == "filesize":
-                
-                # get file name
-                file_path = self.__real_path(datarecv[2])
+            if (datar[0] == "syncs" and datar[1] == "delete"): #delete
+                if (datar[2] == "1"): #delete directory
+                    if os.path.exists(os.path.join(self.working_dir, datar[3])):
+                        os.rmdir(os.path.join(self.working_dir, datar[3]))
+                    self.conn.send("delete|ok".encode("utf-8"))
+                    logging.info("delete success")
 
-                filesize = int(datarecv[4])
+            if (datar[0] == "syncs" and datar[1] == "create"): #Create
+                if (datar[2] == "1"): #Create directory
+                    if not os.path.exists(os.path.join(self.working_dir, datar[3])):
+                        os.makedirs(os.path.join(self.working_dir, datar[3]))
+                    logging.info("Create directory success")
+                    self.conn.send("directory|ok".encode("utf-8"));
+                if (datar[2] == "2"): #Create file
+                    logging.info("Create file")
+                    datarecv = ""
+                    while 1:
+                        data = self.conn.recv(1024)
+                        datarecv += data.decode('utf-8')
+                        if len(datarecv) > 0:
+                            break
 
-                self.conn.send("syncs|ok".encode('utf-8'))
-                f = open(os.path.join(self.working_dir, file_path), "wb")
-                byterecv = 0
-                while 1:
-                    if byterecv == filesize:
-                        break
-                    buff = self.conn.recv(1024)
-                    if len(buff) == 0:
-                        break
-                    f.write(buff)
-                    byterecv += len(buff)
-                f.close()
-                logging.info("Received file %s", file_path)
+                    datarecv = datarecv.split("|")
+
+                    if datarecv[0] =="syncs" and datarecv[1] == "filename" and datarecv[3] == "filesize":
+
+                        # get file name
+                        file_path = self.__real_path(datarecv[2])
+                        logging.info(file_path)
+
+                        filesize = int(datarecv[4])
+
+                        self.conn.send("syncs|ok".encode('utf-8'))
+                        f = open(os.path.join(self.working_dir, file_path), "wb")
+                        byterecv = 0
+
+                        while 1:
+                            if byterecv == filesize:
+                                break
+                            buff = self.conn.recv(1024)
+                            if len(buff) == 0:
+                                break
+                            f.write(buff)
+                            byterecv += len(buff)
+                        f.close()
+                        logging.info("Received file %s", file_path)
 
     def senddata(self):
         return
@@ -92,11 +139,23 @@ class SocketFileClient(object):
     def on_created(self, src_path, is_directory):
         if is_directory:
             #TODO: create new directory
-            pass
+            directory = src_path[src_path.find("\\") + 1 : len(src_path)]
+            buffsend = "syncs|create|1|" + directory
+            logging.info(buffsend)
+            self.sc.send(buffsend.encode('utf-8'))
+
+            buffrecv = self.sc.recv(1024)
+            buffrecv = buffrecv.decode('utf-8')
+            if (buffrecv == "directory|ok"):
+                logging.info("send directory success")
+
         else:
+            buffsend = "syncs|create|2"
+            self.sc.send(buffsend.encode("utf-8"))
+
             filesize = os.path.getsize(src_path)
-            datasend = "syncs|filename|" + src_path + "|filesize|" + str(filesize) + "|\r\n"
-            
+            datasend = "syncs|filename|" + src_path + "|filesize|" + str(filesize)
+
             logging.info(datasend)
             datalen = len(datasend)
             while True:
@@ -106,6 +165,7 @@ class SocketFileClient(object):
             datarecv = self.sc.recv(1024)
             datarecv = datarecv.decode('utf-8')
 
+            logging.info(datarecv)
             if datarecv == "syncs|ok":
                 f = open(src_path, "rb")
                 byteSend = 0
@@ -115,17 +175,31 @@ class SocketFileClient(object):
                     buff = f.read(1024)
                     self.sc.send(buff)
                     byteSend += len(buff)
+            logging.info("Send file success")
 
 
     def on_deleted(self, src_path, is_directory):
-        pass
+        if is_directory:
+            directory = src_path[src_path.find("\\") + 1 : len(src_path)]
+            buffsend = "syncs|delete|1" + directory
+            logging.info(buffsend)
+            self.sc.send(buffsend.encode("utf-8"))
+
+            buffrecv = self.sc.recv(1024)
+            buffrecv = buffrecv.decode('utf-8')
+            if (buffrecv == "delete|ok"):
+                logging.info("send directory success")
+        else:
+            pass
+
+
 
     def on_modified(self, src_path, is_directory):
         pass
 
     def on_moved(self, src_path, dest_path, is_directory):
         pass
-        
+
 
 
 
